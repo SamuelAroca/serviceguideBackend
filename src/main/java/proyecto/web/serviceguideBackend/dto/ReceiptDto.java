60e38ed2a91f62f3779022d53732f8cc9179845f
SamuelAroca <samuel.arocad@unac.edu.co>
2023-04-05 21:11:20 -0500

    update

--- src/main/java/proyecto/web/serviceguideBackend/dto/ReceiptDto.java
+++ src/main/java/proyecto/web/serviceguideBackend/dto/ReceiptDto.java
@@ -14,33 +14,19 @@ import proyecto.web.serviceguideBackend.entities.User;
 @Builder
 public class ReceiptDto {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_receipt")
     private Long id;
 
-    @Column(name = "water_price")
     private Long waterPrice;
-    @Column(name = "water_amount")
     private Long waterAmount;
 
-    @Column(name = "sewerage_price")
     private Long seweragePrice;
-    @Column(name = "sewerage_amount")
     private Long sewerageAmount;
 
-    @Column(name = "energy_price")
     private Long energyPrice;
-    @Column(name = "energy_amount")
     private Long energyAmount;
 
-    @Column(name = "gas_price")
     private Long gasPrice;
-    @Column(name = "gas_amount")
     private Long gasAmount;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "user_id")
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private User user;
 }
