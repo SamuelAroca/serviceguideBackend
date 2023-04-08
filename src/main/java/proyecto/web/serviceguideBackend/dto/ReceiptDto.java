b3e52cbd0ef673cec15c16e31b4ddbbbb94e2248
SamuelAroca <samuel.arocad@unac.edu.co>
2023-04-07 22:04:13 -0500

    Update Warnings

--- src/main/java/proyecto/web/serviceguideBackend/dto/ReceiptDto.java
+++ src/main/java/proyecto/web/serviceguideBackend/dto/ReceiptDto.java
@@ -1,7 +1,5 @@
 package proyecto.web.serviceguideBackend.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- src/main/java/proyecto/web/serviceguideBackend/entities/User.java
+++ src/main/java/proyecto/web/serviceguideBackend/entities/User.java
@@ -7,6 +7,7 @@ import lombok.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("unused")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
