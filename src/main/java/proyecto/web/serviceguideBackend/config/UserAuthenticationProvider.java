7418124d2071c7a71a94930737193dec64901cd5
SamuelAroca <samuel.arocad@unac.edu.co>
2023-04-15 00:21:59 -0500

    Who I am

--- src/main/java/proyecto/web/serviceguideBackend/config/UserAuthenticationProvider.java
+++ src/main/java/proyecto/web/serviceguideBackend/config/UserAuthenticationProvider.java
@@ -7,10 +7,14 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import proyecto.web.serviceguideBackend.dto.UserDto;
+import proyecto.web.serviceguideBackend.entities.User;
+import proyecto.web.serviceguideBackend.exceptions.AppException;
+import proyecto.web.serviceguideBackend.repositories.UserRepository;
 import proyecto.web.serviceguideBackend.services.UserService;
 
 import java.util.Base64;
@@ -25,6 +29,7 @@ public class UserAuthenticationProvider {
     private String secretKey;
 
     private final UserService userService;
+    private final UserRepository userRepository;
 
     @PostConstruct
     protected void init() {
@@ -56,4 +61,17 @@ public class UserAuthenticationProvider {
         return new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
     }
 
+    public String whoIAm(String token) {
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+        JWTVerifier verifier = JWT.require(algorithm)
+                .build();
+
+        DecodedJWT decoded = verifier.verify(token);
+
+        User user = userRepository.findByLogin(decoded.getIssuer())
+                .orElseThrow(() -> new AppException("Not found", HttpStatus.NOT_FOUND));
+
+        return user.getLogin();
+    }
 }
