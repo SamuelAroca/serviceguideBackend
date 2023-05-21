65dc7c024c08377a823766e6efa50847ceae9070
JimDiazC <101286666+JimDiazC@users.noreply.github.com>
2023-05-21 18:18:13 -0500

    Test

--- src/test/java/proyecto/web/serviceguideBackend/ServiceguideBackendApplicationTests.java
+++ src/test/java/proyecto/web/serviceguideBackend/ServiceguideBackendApplicationTests.java
@@ -1,5 +1,7 @@
 package proyecto.web.serviceguideBackend;
 
+import org.checkerframework.checker.nullness.Opt;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,13 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
-import proyecto.web.serviceguideBackend.dto.CredentialsDto;
-import proyecto.web.serviceguideBackend.dto.Message;
-import proyecto.web.serviceguideBackend.dto.UserDto;
-import proyecto.web.serviceguideBackend.entities.City;
-import proyecto.web.serviceguideBackend.entities.House;
-import proyecto.web.serviceguideBackend.entities.User;
-import proyecto.web.serviceguideBackend.serviceInterface.UserInterface;
+import proyecto.web.serviceguideBackend.config.UserAuthenticationProvider;
+import proyecto.web.serviceguideBackend.dto.*;
+import proyecto.web.serviceguideBackend.entities.*;
+import proyecto.web.serviceguideBackend.repositories.UserRepository;
+import proyecto.web.serviceguideBackend.serviceInterface.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -26,14 +26,89 @@ import java.util.Optional;
 class ServiceguideBackendApplicationTests {
 
 	@Autowired
+	private AuthInterface authInterface;
+
+	@Autowired
+	private CityInterface cityInterface;
+
+	@Autowired
+	private HouseInterface houseInterface;
+
+	@Autowired
+	private ReceiptInterface receiptInterface;
+
+	@Autowired
+	private StatisticInterface statisticInterface;
+
+	@Autowired
 	private UserInterface userInterface;
+
+	@Autowired
+	private UserAuthenticationProvider userAuthenticationProvider;
+
+	@Autowired
+	private UserRepository userRepository;
+
 
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	void testLogin() {
+	void testAuthFindByEmail() {
+		String email = "serviceguide23@gmail.com";
+
+		UserDto userDto = authInterface.findByEmail(email);
+
+		Assertions.assertNotNull(userDto);
+		Assertions.assertEquals(email, userDto.getEmail());
+	}
+
+	@Test
+	void testCityListAll() {
+		Collection<City> cityList = cityInterface.listAll();
+
+		Assertions.assertNotNull(cityList);
+	}
+
+	@Test
+	void testCityFindById() {
+		Long id = 1L;
+
+		Optional<City> optionalCity = cityInterface.findById(id);
+		Assertions.assertNotNull(optionalCity);
+	}
+
+	@Test
+	void testCityFindByCity() {
+		String city = "Medellín";
+
+		Optional<City> optionalCity = cityInterface.findByCity(city);
+		Assertions.assertNotNull(optionalCity);
+		Assertions.assertEquals(city, optionalCity.get().getCity());
+	}
+
+	@Test
+	void testStatisticIndividualReceipt() {
+		Long idReceipt = 46L;
+		String typeReceipt = "ENERGY";
+		String typeGraphic = "Bar";
+
+		StatisticDto newStatistic = statisticInterface.individualReceipt(typeReceipt, idReceipt, typeGraphic);
+
+		Assertions.assertNotNull(newStatistic);
+	}
+
+	@Test
+	void testStatisticGetStatisticByReceipt() {
+		Long id = 45L;
+		List<Statistic> statistics = statisticInterface.getStatisticByReceipt(id);
+
+		Assertions.assertNotNull(statistics);
+	}
+
+	@Test
+	void testUserLogin() {
 
 		CredentialsDto credentialsDto = new CredentialsDto("serviceguide23@gmail.com", "1234".toCharArray());
 
@@ -43,16 +118,25 @@ class ServiceguideBackendApplicationTests {
 		Assertions.assertEquals("serviceguide23@gmail.com", userDto.getEmail());
 		Assertions.assertEquals(2L, userDto.getId());
 	}
+	/*
+	@Test
+	void testUserRegister() {
+		SignUpDto signUpDto = new SignUpDto();
+		signUpDto.setFirstName("Prueba");
+		signUpDto.setLastName("Test");
+		signUpDto.setEmail("prueba@gmail.com");
+		signUpDto.setPassword("prueba123".toCharArray());
+
+		UserDto registeredUser = userInterface.register(signUpDto);
+
+		Assertions.assertNotNull(registeredUser.getId());
+		Assertions.assertEquals(signUpDto.getEmail(), registeredUser.getEmail());
+		Assertions.assertEquals(signUpDto.getFirstName(), registeredUser.getFirstName());
+		Assertions.assertEquals(signUpDto.getLastName(), registeredUser.getLastName());
+	}*/
 
 	@Test
-	void testListAll() {
-		Collection<User> userList = userInterface.listAll();
-
-		Assertions.assertNotNull(userList);
-	}
-
-	@Test
-	void testGetByEmail() {
+	void testUserGetByEmail() {
 		String email = "serviceguide23@gmail.com";
 
 		Optional<User> optionalUser = userInterface.getByEmail(email);
@@ -71,8 +155,10 @@ class ServiceguideBackendApplicationTests {
 	void testFindById() {
 
 		CredentialsDto credentialsDto = new CredentialsDto("serviceguide23@gmail.com", "1234".toCharArray());
+		UserDto userDto = userInterface.login(credentialsDto);
+		userDto.setToken(userAuthenticationProvider.createToken(userDto.getEmail()));
 
-		String token = userInterface.login(credentialsDto).getToken();
+		String token = userDto.getToken();
 
 		Optional<User> optionalUser = userInterface.findById(token);
 
@@ -84,11 +170,58 @@ class ServiceguideBackendApplicationTests {
 		Assertions.assertEquals("serviceguide23@gmail.com", user.getEmail());
 		Assertions.assertEquals("Service", user.getFirstName());
 		Assertions.assertEquals("Guide", user.getLastName());
-
 	}
 
-	/*@Test
-	void testSave() {
+	@Test
+	void testUserUpdateUser() {
+		CredentialsDto credentialsDto = new CredentialsDto("prueba4@gmail.com", "prueba123".toCharArray());
+		UserDto userDto = userInterface.login(credentialsDto);
+		userDto.setToken(userAuthenticationProvider.createToken(userDto.getEmail()));
+		String token = userDto.getToken();
+
+		SignUpDto updateUserDto = new SignUpDto();
+		updateUserDto.setFirstName("Prueba Update");
+		updateUserDto.setLastName("Test Update");
+		updateUserDto.setEmail("prueba5@gmail.com");
+		updateUserDto.setPassword("prueba123".toCharArray());
+
+		Optional<UpdateUserDto> updatedUserDto = userInterface.updateUser(updateUserDto, token);
+		Assertions.assertEquals("User updated", updatedUserDto.get().getMessage());
+
+		User updatedUser = userRepository.findByEmail(updateUserDto.getEmail()).orElse(null);
+
+		Assertions.assertEquals(updateUserDto.getFirstName(), updatedUser.getFirstName());
+		Assertions.assertEquals(updateUserDto.getLastName(), updatedUser.getLastName());
+	}
+
+	@Test
+	void testUserListAll() {
+		Collection<User> userList = userInterface.listAll();
+
+		Assertions.assertNotNull(userList);
+	}
+	/*
+	@Test
+	void testUserFindByTokenPassword() {
+		CredentialsDto credentialsDto = new CredentialsDto("serviceguide23@gmail.com", "1234".toCharArray());
+		UserDto userDto = userInterface.login(credentialsDto);
+		String tokenPassword = userDto.getToken();
+
+
+		Optional<User> optionalUser = userInterface.findByTokenPassword(tokenPassword);
+
+		Assertions.assertTrue(optionalUser.isPresent());
+
+		User user = optionalUser.orElse(null);
+		Assertions.assertNotNull(user);
+
+		Assertions.assertEquals("serviceguide23@gmail.com", user.getEmail());
+		Assertions.assertEquals("Service", user.getFirstName());
+		Assertions.assertEquals("Guide", user.getLastName());
+	}*/
+	/*
+	@Test
+	void testUserSave() {
 		User user = new User();
 		user.setFirstName("Javier");
 		user.setLastName("Cordoba");
@@ -129,14 +262,17 @@ class ServiceguideBackendApplicationTests {
 		Assertions.assertEquals(2, savedUser.getHouse().size());
 	}*/
 
-	/*@Test
-	void testDelete() {
-		String token = "user_token";
+	@Test
+	void testUserDelete() {
+		CredentialsDto credentialsDto = new CredentialsDto("prueba5@gmail.com", "prueba123".toCharArray());
+		UserDto userDto = userInterface.login(credentialsDto);
+		userDto.setToken(userAuthenticationProvider.createToken(userDto.getEmail()));
+		String token = userDto.getToken();
 
 		Message deleteMessage = userInterface.delete(token);
 
 		Assertions.assertNotNull(deleteMessage);
-		Assertions.assertEquals("User deleted", deleteMessage.getMessage());
-	}*/
+		Assertions.assertEquals("Delete success", deleteMessage.getMessage());
+	}
 
 }
