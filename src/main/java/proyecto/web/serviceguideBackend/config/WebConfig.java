0141f359a1133d4c2251e16ff0b6f2d36e6b09a8
SamuelAroca <samuel.arocad@unac.edu.co>
2023-05-27 00:13:28 -0500

    Update WebConfig.java

--- src/main/java/proyecto/web/serviceguideBackend/config/WebConfig.java
+++ src/main/java/proyecto/web/serviceguideBackend/config/WebConfig.java
@@ -20,15 +20,15 @@ public class WebConfig {
     private static final Long MAX_AGE = 3600L;
     private static final int CORS_FILTER_ORDER = -102;
 
-    @Value("${front.URL}")
-    private String URL;
+    /*@Value("${front.URL}")
+    private String URL;*/
 
     @Bean
     public FilterRegistrationBean<CorsFilter> corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin(URL);
+        config.addAllowedOrigin("http://89.116.25.43:5002");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
